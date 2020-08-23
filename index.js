b1a95c5acd2ea48147a612eb12e9fd7124cf7138
chen-tan <1154638108@qq.com>
2020-08-23 12:35:43 +0800

    添加了点击变大变绿的功能

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-alert('dksjflsdjfklds')
+alert('欢迎来到阿坛的界面');
+alert('你点击了红色方块');
+var div=document.getElementsByTagName('div');
+div.onclick=function(e){
+    var tar=e.target;
+    tar.className='change';
+}
