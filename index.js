655b2647140a553b43ef59c39de76f0e57d220c1
chen-tan <1154638108@qq.com>
2020-08-23 12:39:37 +0800

    刚才获取元素写错了

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 alert('欢迎来到阿坛的界面');
-alert('你点击了红色方块');
-var div=document.getElementsByTagName('div');
+
+var div=document.getElementsByTagName('div')[0];
+console.log(div);
 div.onclick=function(e){
+    console.log('xxx');
     var tar=e.target;
     tar.className='change';
 }
